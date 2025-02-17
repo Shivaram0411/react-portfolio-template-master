ba8288ac43bc4c7992ad01763bbe6bc45ca63542
Shivaram0411 <144825629+Shivaram0411@users.noreply.github.com>
2025-02-16 19:15:07 -0500

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const webpack = require("webpack");
+
+module.exports = function override(config) {
+  config.resolve.fallback = {
+    crypto: require.resolve("crypto-browserify"),
+    stream: require.resolve("stream-browserify"),
+    buffer: require.resolve("buffer"),
+    assert: require.resolve("assert"),
+    vm: require.resolve("vm-browserify"),
+    url: require.resolve("url") // ✅ Fix for missing 'url/' module
+  };
+
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+      Buffer: ["buffer", "Buffer"],
+    })
+  );
+
+  return config;
+};
