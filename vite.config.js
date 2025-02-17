fad4cc7b04d028813ad4915df2694c86bdb41f0c
Shivaram0411 <144825629+Shivaram0411@users.noreply.github.com>
2025-02-16 19:43:04 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", // ✅ Ensures Vite builds to the correct directory
+    target: "esnext",
+  },
   server: {
     port: 3000,
-  },
-  build: {
-    outDir: "dist",
   },
 });
